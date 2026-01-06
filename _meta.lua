bfb4f7464b18228abf5fa08706d73eedd5ded12d
zeeyado <zzz@zzzs-MacBook-Air.local>
2026-01-06 16:32:26 +0000

    locale for translations

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
     name = "koassistant",
     fullname = _("KOAssistant"),
-    description = _([[A powerful plugin that lets you interact with AI language models (Claude, GPT-4, etc.) while reading. Ask questions about text, get translations, summaries, explanations and more.]]),
-    version = "0.7.0",
+    description = _([[A powerful AI assistant integrated into KOReader. Technical research assistant and knowledge expander.]]),
+    version = "0.7.1",
 }
