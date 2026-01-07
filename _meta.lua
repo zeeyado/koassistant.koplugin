18eec256cedc03cbcae8afeadb77a2b53e94b57a
zeeyado <zeeyado@pm.me>
2026-01-07 12:34:10 +0000

    version bump

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "koassistant",
     fullname = _("KOAssistant"),
     description = _([[A powerful AI assistant integrated into KOReader. Technical research assistant and knowledge expander.]]),
-    version = "0.7.1",
+    version = "0.8.0",
 }
