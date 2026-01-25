e4443876d8b0fcf7480ac6756f16553063e8e6f8
zeeyado <zzz@zzzs-MacBook-Air.local>
2026-01-25 18:33:14 +0000

    Add tier system and documentation URLs to model_lists.lua

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "koassistant",
     fullname = _("KOAssistant"),
     description = _([[A powerful AI assistant integrated into KOReader. Technical research assistant and knowledge expander.]]),
-    version = "0.11.2",
+    version = "0.11.3-dev",
 }
