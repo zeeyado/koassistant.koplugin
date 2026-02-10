fec52a5654822c562a615b1fca1a66f14804f2e2
zeeyado <zeeyado@pm.me>
2026-02-10 18:15:32 +0000

    repkace var _ loops

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "koassistant",
     fullname = _("KOAssistant"),
     description = _([[A powerful AI assistant integrated into KOReader. Technical research assistant and knowledge expander.]]),
-    version = "0.17.3",
+    version = "0.17.4-dev",
 }
